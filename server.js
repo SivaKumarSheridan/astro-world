97e0c7486852be19cbadc29a9e95d3d8eb8b4119
Siva <sivaskvn@gmail.com>
2022-03-17 16:43:38 -0400

    check

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const serverStatic = require('serve-static');
 const path = require('path');
 
 const app = express();
-
+app.use('/', serverStatic(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 8080
 app.listen(port);
